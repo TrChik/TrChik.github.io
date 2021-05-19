0b7802b86269c9fb21bc9528695fb732a1f5856f
TrChik <trchik14@gmail.com>
2021-05-19 15:36:26 +0300

    upd

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,24 @@ function menuswitch() {
     x.style.display = "block";
   }
 }
+
+function searchbar() {
+  var input, filter, table, tr, th, i, txtvalue;
+  input = document.getElementById("input1");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("table1");
+  tr = table.getElementsByTagName("tr");
+  for(i = 0; i < tr.length; i = i + 2){
+    th = tr[i].getElementsByTagName("th")[0];
+    if(th) {
+      txtvalue = th.textContent || th.innerText;
+      if(txtvalue.toLowerCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        tr[i+1].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+        tr[i+1].style.display = "none";
+      }
+    }
+  }
+}
